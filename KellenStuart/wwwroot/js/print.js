053bdbb149b860a9bbe5c5e9d2a2d47a6941ff2d
Kellen Stuart <kellen.m.stuart@gmail.com>
2021-10-22 12:29:25 -0600

    significantly improve my resume

--- KellenStuart/wwwroot/js/print.js
+++ KellenStuart/wwwroot/js/print.js
@@ -1,3 +1,14 @@
 ﻿function PrintResume() {
     window.print();
 }
+
+function FocusJP()
+{
+    var jp = document.getElementById("john-peterson");
+    jp.focus();
+    jp.classList.add("gradient-border");
+    setTimeout(function() {
+        jp.classList.remove("gradient-border");
+    }, 1200)
+    console.log("FocusJP clicked")
+}
