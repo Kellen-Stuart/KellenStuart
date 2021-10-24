cdade15a809c5f2d2dd61ac89eb76c9ed3408202
Kellen Stuart <kellen.m.stuart@gmail.com>
2021-10-24 08:42:47 -0600

    abstract stuff into a base library

---
+++ KellenStuart/Shared/ArticleBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KellenStuart.Shared
+{
+    public class ArticleBase : Base 
+    {
+        protected const string Content = "col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12";
+    }
+}
---
+++ KellenStuart/Shared/Base.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace KellenStuart.Shared
+{
+    public class Base : ComponentBase
+    {
+        public static Dictionary<string, string> Urls { get; set; }
+
+        public Base()
+        {
+            Urls = new Dictionary<string, string>();
+            Urls["WSCU"] = "https://western.edu/";
+            Urls["CompTIASecurityPlus"] = "https://www.comptia.org/certifications/security";
+            Urls["LockheedMartin"] = "https://www.lockheedmartin.com/en-us/index.html";
+            Urls["JohnPetersonObituary"] = "https://archive.gunnisontimes.com/obituaries/john-peterson";
+            Urls["HaskellReport"] = "https://www.haskell.org/onlinereport/";
+            Urls["GentleIntroductionToHaskell"] = "https://www.haskell.org/tutorial/";
+            Urls["YampaLibrary"] = "https://hackage.haskell.org/package/Yampa";
+            Urls["Home"] = "https://kellenstuart.com";
+            Urls["Resume"] = "https://kellenstuart.com/resume";
+        }
+    }
+}
